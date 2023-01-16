2adcc6ce4ebb33b342b4c191ce4e78b29ce250e6
Qadir <react1@flyweis.technolgy>
2023-01-16 06:48:00 -0800

    changes

--- src/Component/HomeScreen2.js
+++ src/Component/HomeScreen2.js
@@ -1,39 +1,92 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../Assets/Navbar";
 import Image2 from "./Group 537.png";
+import logo from "../Images/rrh-logo 3.png";
+import SearchModal from "../Modals/SearchModal";
+import PromocodeModal from "../Modals/Promocode";
+import ReissueCoupon from "../Modals/ReissueCoupon";
 
 const HomeScreen2 = () => {
+  const navigate = useNavigate();
+  const [slide, setSlide] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [Reissue, setRessiue] = useState(false);
+
   return (
     <>
+      <SearchModal show={open} onHide={() => setOpen(false)} />
+
+      <PromocodeModal show={modalShow} onHide={() => setModalShow(false)} />
+
+      <ReissueCoupon show={Reissue} onHide={() => setRessiue(false)} />
+
       <NavBar />
 
-
       <div className="home2New">
-        <div className="outer">
+        <div
+          className="outer"
+          onClick={() => {
+            setSlide(false);
+            navigate("/search");
+          }}
+        >
           <img src={Image2} alt="" />
           <p>Issue Coupon Offers</p>
         </div>
-        <div className="outer">
+        <div
+          className="outer"
+          onClick={() => {
+            setModalShow(false);
+            setRessiue(true);
+            setSlide(false);
+          }}
+        >
           <img src={Image2} alt="" />
           <p>Coupon Reissue</p>
         </div>
-        <div className="outer">
+        <div
+          className="outer"
+          onClick={() => {
+            setOpen(true);
+            setSlide(false);
+          }}
+        >
           <img src={Image2} alt="" />
-          <p>Search Coupon</p>
+          <p>Search and Redeem Coupon</p>
         </div>
       </div>
       <div className="home2New">
-        <div className="outer">
+        <div
+          className="outer"
+          onClick={() => {
+            setSlide(false);
+            navigate("/redeem");
+          }}
+        >
           <img src={Image2} alt="" />
           <p>Reedem Coupon</p>
         </div>
-        <div className="outer">
+        <div
+          className="outer"
+          onClick={() => {
+            setSlide(false);
+            navigate("/summary");
+          }}
+        >
           <img src={Image2} alt="" />
           <p>Summary</p>
         </div>
-        <div className="outer">
+        <div
+          className="outer"
+          onClick={() => {
+            setSlide(false);
+            navigate("/");
+          }}
+        >
           <img src={Image2} alt="" />
           <p>Logout</p>
         </div>
